ccd09aea2a67495c4620c4c2b010182a5bb00cf8
mrmissouri <dwayne.missouri@gmail.com>
2018-04-18 14:16:32 -0500

    Timer2 program file push

---
+++ Timer2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please type Start, to start the timer.");
+        string input = Console.ReadLine().ToLower();
+
+        while (input != "quit")
+        {
+
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            if (input == "start")
+            {
+                stopwatch.Start();
+                Console.WriteLine("Please type Stop, to stop your timer and get elapsed time.");
+                input = Console.ReadLine().ToLower();
+
+                if (input == "stop")
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.Elapsed);
+                    Console.WriteLine("Please type start, to start a new timer, or quit, " +
+                            "to quit the timer application.");
+                    input = Console.ReadLine().ToLower();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please type Start, to start the timer.");
+                input = Console.ReadLine().ToLower();
+            }
+        }
+
+    }
+
+}
